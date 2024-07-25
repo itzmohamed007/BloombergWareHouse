e76a8e2b91c2aa9f3bd72a632017a546741eea95
mohamed bourra <itzmohamed007@gmail.com>
2024-07-25 15:28:48 +0100

    init

---
+++ src/main/java/dev/bloomberg/warehouse/BloombergWareHouseApplication.java
@@ -0,0 +1,13 @@
+package dev.bloomberg.warehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloombergWareHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloombergWareHouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bloomberg/warehouse/BloombergWareHouseApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bloomberg.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloombergWareHouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
