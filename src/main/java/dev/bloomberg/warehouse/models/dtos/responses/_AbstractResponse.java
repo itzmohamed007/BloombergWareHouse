1ee122cd7dacf9bdd55dbc82fee6193fb2bd3498
mohamed bourra <itzmohamed007@gmail.com>
2024-07-25 19:10:40 +0100

    added abstract response dto to group duplicate fields

---
+++ src/main/java/dev/bloomberg/warehouse/models/dtos/responses/_AbstractResponse.java
@@ -0,0 +1,33 @@
+package dev.bloomberg.warehouse.models.dtos.responses;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import dev.bloomberg.warehouse.models.entities._AbstractEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+import java.sql.Timestamp;
+
+/**
+ * Base class for response DTOs representing {@link _AbstractEntity}.
+ * AbstractResponse includes common fields such as id, createdAt, updatedAt, and version.
+ * This class is intended to be extended by specific response DTOs in the system.
+ */
+@Getter
+@Setter
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public abstract class _AbstractResponse implements _Response<Long> {
+    /**
+     * The unique identifier of the entity.
+     */
+    private Long id;
+    /**
+     * The timestamp when the entity was created.
+     */
+    private Timestamp timestamp;
+}
