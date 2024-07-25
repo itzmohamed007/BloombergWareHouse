420f6c28c8ac6a1a6b0fef1fb21103fda0ed2ed4
mohamed bourra <itzmohamed007@gmail.com>
2024-07-25 19:15:21 +0100

    added FxDeal service interface

---
+++ src/main/java/dev/bloomberg/warehouse/services/spec/IFxDealService.java
@@ -0,0 +1,9 @@
+package dev.bloomberg.warehouse.services.spec;
+
+
+import dev.bloomberg.warehouse.models.dtos.requests.FxDealRequest;
+import dev.bloomberg.warehouse.models.dtos.responses.FxDealResponse;
+
+public interface IFxDealService extends _Service<Long, FxDealRequest, FxDealResponse> {
+
+}
